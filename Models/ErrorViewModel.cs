cc30ded8a2090d7823e76d836d4173a59e7016fc
abukhmirov <alexbukhmirov@gmail.com>
2023-12-14 12:36:49 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proof_of_Deployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
